741df50e8cb5652a5952c2947ef1832864c3a187
Izzatbek <abdusharipovizzat03@gmail.com>
2026-05-05 22:41:00 +0500

    Authentication

--- src/main/java/com/sms/authentication/util/MailUtil.java
+++ src/main/java/com/sms/authentication/util/MailUtil.java
@@ -1,6 +1,5 @@
 package com.sms.authentication.util;
 
-import com.sms.authentication.constant.Endpoint;
 import com.sms.authentication.dto.mail.EmailPayload;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
@@ -10,12 +9,14 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
+import static com.sms.authentication.config.RabbitMQConfig.QUEUE_NAME;
+
 @Component
 @RequiredArgsConstructor
 public class MailUtil {
     private final JavaMailSender javaMailSender;
 
-    @RabbitListener(queues = Endpoint.QUEUE_NAME)
+    @RabbitListener(queues = QUEUE_NAME)
     public void sendMail(EmailPayload payload) throws MessagingException {
         MimeMessage message = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message,true,"UTF-8");
